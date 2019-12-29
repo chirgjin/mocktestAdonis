f6273b075cf573e632fc3d9ae4d483e1614007b6
Chirgjin <chirgjin@gmail.com>
2019-12-29 15:33:35 +0530

    ensure only 1 row in setting table

--- database/migrations/1560069845090_settings_schema.js
+++ database/migrations/1560069845090_settings_schema.js
@@ -6,7 +6,8 @@ const Schema = use('Schema');
 class SettingsSchema extends Schema {
     up () {
         this.create('settings', (table) => {
-            table.increments();
+            // table.increments();
+            table.enu('id', [1]).primary().defaultTo(1);
             table.string('name', 150);
             table.string('image', 200);
             table.integer('max_users').defaultTo(100);
