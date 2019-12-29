ed83228220206c807f207396a65bb45fd43b8e40
Chirgjin <chirgjin@gmail.com>
2019-12-29 14:54:08 +0530

    basic stats api

--- app/Controllers/Http/Admin/MiscController.js
+++ app/Controllers/Http/Admin/MiscController.js
@@ -3,6 +3,8 @@
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 
+const { Test, UserTest, Question, User, Setting } = use("App/Models");
+
 // const Difficulty = use("App/Models/Difficulty");
 class MiscController {
 
@@ -15,8 +17,21 @@ class MiscController {
     * @param {Response} ctx.response
     */
     async home ({ response }) {
+        const setting = await Setting.query().first();
+
         return response.success({
-            stats : "to be added",
+            users : {
+                created : await User.query().getCount(),
+                max : setting && setting.max_users || null,
+            },
+            tests : {
+                created : await Test.query().getCount(),
+                taken : await UserTest.query().getCount(),
+            },
+            questions : await Question.query().getCount(),
+            image : setting && setting.image || null,
+            name : setting && setting.name || null,
+            active_to : setting && setting.active_to || null
         });
     }
 }
